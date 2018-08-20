918b101220ab7ccfd6e847b086393302c2076798
xqin1 <qinxm2000@yahoo.com>
2018-08-19 22:22:01 -0400

    add task detail page

--- src/app/modules/document-management/components/portal/task-list-item/task-list-item.component.ts
+++ src/app/modules/document-management/components/portal/task-list-item/task-list-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import {Task} from '@app/core/model/workfront/Task.model';
 
 @Component({
   selector: 'app-task-list-item',
@@ -6,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-list-item.component.css']
 })
 export class TaskListItemComponent implements OnInit {
-
+  @Input() taskData: Task;
+  @Input() selectedTaskId: string;
+  @Output() selectTask = new EventEmitter<Task>();
   constructor() { }
 
+  onSelectTask(task: Task) {
+    this.selectTask.emit(task);
+  }
+
+  getShowTaskLink(task: Task) {
+    console.log('show task link');
+  }
+
+  getShowProjectLink(task: Task) {
+    console.log('show project link');
+  }
+
+  getTaskState(task: Task) {
+    console.log('show task state');
+  }
   ngOnInit() {
   }
 

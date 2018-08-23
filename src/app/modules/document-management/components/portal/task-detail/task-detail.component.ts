83420b196b63e640f7de22f092200af9037f26d5
xqin1 <qinxm2000@yahoo.com>
2018-08-23 19:45:45 -0400

    sytle task detail

--- src/app/modules/document-management/components/portal/task-detail/task-detail.component.ts
+++ src/app/modules/document-management/components/portal/task-detail/task-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { TaskData} from '@app/modules/document-management/model/task-data.model';
 import {environment} from '@env/environment';
+import { MatExpansionPanel} from '@angular/material';
+import {EventTargetLike, FromEventTarget} from 'rxjs/internal/observable/fromEvent';
 
 @Component({
   selector: 'app-task-detail',
@@ -15,6 +17,22 @@ export class TaskDetailComponent implements OnInit {
     return `${environment.workfrontHost}/task/view?ID=${taskId}`;
   }
 
+  refreshTask() {
+    console.log("refresh task");
+  }
+
+  expandPanel(matExpansionPanel: MatExpansionPanel, event: Event): void {
+    event.stopPropagation(); // Preventing event bubbling
+
+    if (!this._isExpansionIndicator(event)) {
+      matExpansionPanel.toggle(); // Here's the magic
+    }
+  }
+
+  private _isExpansionIndicator(event: Event): boolean {
+    const expansionIndicatorClass = 'mat-expansion-indicator';
+    return (event.target["classList"] && event.target["classList"].contains(expansionIndicatorClass) );
+  }
   ngOnInit() {
   }
 

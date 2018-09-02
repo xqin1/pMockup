9c2c426cca6bca276f3c6b9e4a67c1383291a84c
xqin1 <qinxm2000@yahoo.com>
2018-09-02 18:37:06 -0400

    add notification bar

--- src/app/modules/document-management/containers/portal/document-portal-parent-page/document-portal-parent-page.component.ts
+++ src/app/modules/document-management/containers/portal/document-portal-parent-page/document-portal-parent-page.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import { TaskListResolverService} from '@app/modules/document-management/services/task-list-resolver.service';
 import { Observable} from 'rxjs/index';
 import { select, Store } from '@ngrx/store';
 import * as fromTask from '@app/modules/document-management/reducers/index.reducer';
+import {MatSnackBar, MatSnackBarConfig} from '@angular/material';
 
 @Component({
   selector: 'app-document-portal-parent-page',
@@ -12,18 +13,37 @@ import * as fromTask from '@app/modules/document-management/reducers/index.reduc
 export class DocumentPortalParentPageComponent implements OnInit {
   taskListLoading$: Observable<boolean>;
   taskListLoaded$: Observable<boolean>;
-
+  taskLoadIds$: Observable<string[]>;
+  config: MatSnackBarConfig = new MatSnackBarConfig();
   constructor(
     private taskListResolveService: TaskListResolverService,
-    private store: Store<fromTask.State>
+    private store: Store<fromTask.State>,
+    public snackBar: MatSnackBar
   ) {
     this.taskListLoading$ = this.store.pipe(select(fromTask.getTaskListLoading));
     this.taskListLoaded$ = this.store.pipe(select(fromTask.getTaskListLoaded));
-
+    this.taskLoadIds$ = this.store.pipe(select(fromTask.getTaskLoadIds));
+    this.config.verticalPosition = "top";
+    this.config.panelClass = ['snack-bar'];
   }
+
 
   ngOnInit() {
     this.taskListResolveService.resolve();
+    this.taskLoadIds$.subscribe(result => {
+      if (result.length > 0) {
+        this.snackBar.open("Retrieving Task Information...", "" , this.config);
+      }else{
+        this.snackBar.dismiss();
+      }
+    });
+    // this.taskListLoading$.subscribe(result => {
+    //   if (result !== null && typeof result !== 'undefined' && result) {
+    //     this.snackBar.open("Retrieving Tasks Information...", "" , this.config);
+    //   }else{
+    //     this.snackBar.dismiss();
+    //   }
+    // });
   }
 
 }
